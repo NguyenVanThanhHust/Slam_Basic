ca057374050b8356aa7797387d1eee3ffcea9a23
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-07-10 15:58:34 +0700

    add chapter 5

---
+++ chapter_5/imageBasic/imageBasic.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char **argv)
+{
+    cv::Mat image;
+    image = cv::imread(argv[1]);
+    if(image.data == nullptr)
+    {
+        cerr<<"This image doesn't exist."<<endl;
+        return 0;
+    }
+    cout<<"number of column: "<<image.cols<<", number of rows: "<<", number of channels"<<image.channels()<<endl;
+    cv::waitKey(0);
+    
+    // Determine type of image
+    if (image.type() != CV_8CU1 && image.type() != CV_8UC3)
+    {
+        cout<<"This is not grayscale or color image"<<endl;
+    }
+    
+    // Traversing the image
+    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    for(size_t y = 0; i <image.rows; y++)
+    {
+        // Get the line pointer with cv::Mat::ptr
+        // row_ptr is the head pointer of the yth line
+        unsigned char *row_ptr = image.ptr<unsigned char>(y);
+        for(size_t x = 0; x <image.cols; x++)
+        {
+            // access value of pixel(x,y) 
+            // data_ptr point to data to be accessed
+            unsigned char *data_ptr = &row_ptr[x * image.channels()];
+            // output each channel of the pixel, if there is only one channel in grayscale
+            for(int c = 0; c != image.channels(); c++)
+            {
+                unsigned char data = data_ptr[c];
+            }
+        }
+    }
+    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+    chrono::duration<double> time_used = chrono::duration_cast < chrono::duration < double >> (t2 - t1);
+    cout<<"Time to traverse all data points in image: "<<time_used.count() << " ms" <<endl;
+    cv::destroyAllWindows();
+    return 0;
+}
---
+++ chapter_5/imageBasic/undistortImage.cpp
@@ -0,0 +1,48 @@
+#include <opencv2/opencv.hpp>
+#include <string>
+
+using namespace std;
+
+string image_file = "./distorted.png";
+
+int main()
+{
+    /*
+    This program implements the code for the distorted part. Although we can call OpenCV's de-distortion, it's helpful to understand it yourself.
+    */
+    double k1 = -0.28340811, k2 = 0.07395907, p1 = 0.00019359, p2 = 1.76187114e-05;
+    // Internal reference
+    double fx = 458.654, fy = 457.296, cx = 367.215, cy = 248.375;
+    cv::Mat image = cv::imread(image_file, 0); // The image is a grayscale image, CV_8UC1
+    int rows = image.rows, cols = image.cols;
+    cv::Mat image_undistort = cv::Mat(rows, cols, CV_8UC1); // Calculate the content of the image after dedistortion
+    for (int v = 0; v < rows; v++) 
+    {
+        for (int u = 0; u < cols; u++)
+        {
+            /*
+            According to the formula, the calculated point (u, v) corresponds to the coordinates in the distorted image (u_distorted, v_distorted)
+            */
+            double x = (u - cx) / fx, y = (v - cy) / fy;
+            double r = sqrt(x * x + y * y);
+            double x_distorted = x * (1 + k1 * r * r + k2 * r * r * r * r) + 2 * p1 * x * y + p2 * (r * r + 2 * x * x);
+            double y_distorted = y * (1 + k1 * r * r + k2 * r * r * r * r) + p1 * (r * r + 2 * y * y) + 2 * p2 * x * y;
+            double u_distorted = fx * x_distorted + cx;
+            double v_distorted = fy * y_distorted + cy;
+            // Assignment (nearest neighbor interpolation)
+            if (u_distorted >= 0 && v_distorted >= 0 && u_distorted < cols && v_distorted < rows)
+            {
+                image_undistort.at<uchar>(v, u) = image.at<uchar>((int) v_distorted, (int) u_distorted);
+            }
+            else
+            {
+                image_undistort.at<uchar>(v, u) = 0;
+            }   
+        }
+    }
+    cv::imshow("distorted", image);
+    cv::imshow("undistorted", image_undistort);
+    cv::imwrite("undistorted_image.jpg", image_undistort);
+    cv::waitKey();
+    return 0;
+}
