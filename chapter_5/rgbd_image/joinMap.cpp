fa271fc0541099e4da5c0d0f1de8193b8f107cdb
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-07-10 19:56:07 +0700

    update joinMap.cpp

--- chapter_5/rgbd_image/joinMap.cpp
+++ chapter_5/rgbd_image/joinMap.cpp
@@ -9,25 +9,27 @@ using namespace std;
 typedef vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> TrajectoryType;
 typedef Eigen::Matrix<double, 6, 1> Vector6d;
 
-// 在pangolin中画图，已写好，无需调整
-void showPointCloud(
-    const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointcloud);
+// Draw a picture in pangolin, it has been written, no need to adjust
+void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointCloud);
 
-int main(int argc, char **argv) {
-    vector<cv::Mat> colorImgs, depthImgs;    // 彩色图和深度图
-    TrajectoryType poses;         // 相机位姿
-
+int main()
+{
+    vector<cv::Mat> colorImgs, depthImgs;
+    TrajectoryType poses; // camera pose
+    
     ifstream fin("./pose.txt");
     if (!fin) {
         cerr << "请在有pose.txt的目录下运行此程序" << endl;
         return 1;
     }
-
-    for (int i = 0; i < 5; i++) {
-        boost::format fmt("./%s/%d.%s"); //图像文件格式
+    
+    for(int i = 0; i < 5, i++)
+    {
+        boost::format fmt("./%s/%d.%s");
         colorImgs.push_back(cv::imread((fmt % "color" % (i + 1) % "png").str()));
-        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
-
+        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1));
+        // read the original image with -1
+        
         double data[7] = {0};
         for (auto &d:data)
             fin >> d;
@@ -35,9 +37,7 @@ int main(int argc, char **argv) {
                           Eigen::Vector3d(data[0], data[1], data[2]));
         poses.push_back(pose);
     }
-
-    // 计算点云并拼接
-    // 相机内参 
+    // Calculate the point cloud and splicing camera internal reference
     double cx = 325.5;
     double cy = 253.5;
     double fx = 518.0;
@@ -45,16 +45,23 @@ int main(int argc, char **argv) {
     double depthScale = 1000.0;
     vector<Vector6d, Eigen::aligned_allocator<Vector6d>> pointcloud;
     pointcloud.reserve(1000000);
-
-    for (int i = 0; i < 5; i++) {
-        cout << "转换图像中: " << i + 1 << endl;
+    
+    for(int i = 0 ; i < 5; i++)
+    {
+        cout<<"convert image: "<<(i+1)<<endl;
         cv::Mat color = colorImgs[i];
         cv::Mat depth = depthImgs[i];
-        Sophus::SE3d T = poses[i];
         for (int v = 0; v < color.rows; v++)
-            for (int u = 0; u < color.cols; u++) {
-                unsigned int d = depth.ptr<unsigned short>(v)[u]; // 深度值
-                if (d == 0) continue; // 为0表示没有测量到
+        {
+            for (int u = 0; u < color.cols; u++)
+            {
+                // depth value
+                unsigned int d = depth.ptr<unsigned short>(v)[u]; 
+                // 0 means no measurement
+                if(d == 0 )
+                {
+                    continue;
+                }
                 Eigen::Vector3d point;
                 point[2] = double(d) / depthScale;
                 point[0] = (u - cx) * point[2] / fx;
@@ -68,12 +75,14 @@ int main(int argc, char **argv) {
                 p[3] = color.data[v * color.step + u * color.channels() + 2]; // red
                 pointcloud.push_back(p);
             }
+        }
     }
-
-    cout << "点云共有" << pointcloud.size() << "个点." << endl;
+    
+    cout << "Point cloud contains: " << pointcloud.size() << " points." << endl;
     showPointCloud(pointcloud);
-    return 1;
+    return 0;
 }
+
 
 void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointcloud) {
 
@@ -112,5 +121,5 @@ void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &
         pangolin::FinishFrame();
         usleep(5000);   // sleep 5 ms
     }
-    return ;
+    return;
 }
