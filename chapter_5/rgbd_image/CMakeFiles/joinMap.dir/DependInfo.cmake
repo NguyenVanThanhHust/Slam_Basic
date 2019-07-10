a16857d5b8ffd4363bd63fa7d5dd46231ff332b7
Nguyen_Van_Thanh <NguyenVanThanhHust@gmail.com>
2019-07-10 17:20:22 +0700

    add chapter 4 after build sophus successfully

---
+++ chapter_5/rgbd_image/CMakeFiles/joinMap.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/thanh/New Volume/Project/Slam_Basic/chapter_5/rgbd_image/joinMap.cpp" "/media/thanh/New Volume/Project/Slam_Basic/chapter_5/rgbd_image/CMakeFiles/joinMap.dir/joinMap.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/thanh/Sophus"
+  "/usr/local/include/eigen3"
+  "/home/thanh/Pangolin/include"
+  "/home/thanh/Pangolin/build/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
