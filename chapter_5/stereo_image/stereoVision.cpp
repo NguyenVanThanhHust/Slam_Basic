ea7aee1c91a04599e3543dcb3759e6eef23f09c6
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-07-11 11:13:08 +0700

    add stereoVision and example of CMakeLists.txt

---
+++ chapter_5/stereo_image/stereoVision.cpp
@@ -0,0 +1,91 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <Eigen/Core>
+#include <pangolin/pangolin.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace Eigen;
+
+string left_file = "./left.png"
+string right_file = "./right.png"
+
+// Draw a picture in pangoline
+void showPointCloud(const vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> &pointCloud);
+
+int main()
+{
+    // Internal reference parameter
+    double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
+    // Baseline
+    double b = 0.573;
+    
+    cv::Mat left = cv::imread(left_file, 0);
+    cv::Mat right = cv::imread(right_file, 0);
+    
+    // magical parameters
+    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
+        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32); 
+        
+        
+    cv::Mat disparity_sgbm, disparity;
+    sgbm->compute(left, right, disparity_sgbm);
+    disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
+    
+    // Generate a cloud pointCloud
+    ector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> pointCloud;
+    for(int v= 0; v < left.rows; v++)
+    {
+        for(int u = 0; u < left.cols; u++)
+        {
+            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
+            // The front three dimensions are xyz and the fourth dimension is color
+            Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); 
+            // Calculate the position of the point based on the binocular model
+            double x = (u - cx) / fx;
+            double y = (v - cy) / fy;
+            double depth = fx * b / (disparity.at<float>(v, u));
+            point[0] = x * depth;
+            point[1] = y * depth;
+            point[2] = depth;
+            pointcloud.push_back(point);
+        }
+    }
+    cv::imshow("disparity", disparity / 96.0);
+    cv::waitKey(0);
+    
+    showPointCloud(pointcloud);
+    return 0;
+}
+
+void showPointCloud(const vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> &pointCloud)
+{
+    if (!pointCloud.empty())
+    {
+        cout<<"Point cloud is empty!"<<endl;
+    }
+    
+    pangolin::CreateWindowAndBind("Point Cloud Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    while (pangolin::ShouldQuit() == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+        glPointSize(2);
+        glBegin(GL_POINTS);
+        for (auto &p: pointcloud) {
+            glColor3f(p[3], p[3], p[3]);
+            glVertex3d(p[0], p[1], p[2]);
+        }
+        glEnd();
+        pangolin::FinishFrame();
+        usleep(5000);   // sleep 5 ms
+    }
+    return;
+}
