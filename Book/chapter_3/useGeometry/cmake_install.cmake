6f8368f89079bc4b0edafb488ac0bb5be065a3e3
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2021-05-02 14:56:11 +0000

    remove cmake file

--- Book/chapter_3/useGeometry/cmake_install.cmake
+++ Book/chapter_3/useGeometry/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /media/thanh/New Volume/Project/Slam_Basic/chapter_3/useGeometry
+# Install script for directory: /home/ubuntu/projects/Slam_Basic/Book/chapter_3/useGeometry
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/media/thanh/New Volume/Project/Slam_Basic/chapter_3/useGeometry/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ubuntu/projects/Slam_Basic/Book/chapter_3/useGeometry/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
